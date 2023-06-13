d347f3c97151b58d993cc3c3c8cb2c657cdfb180
ManuelSpinola <mspinola10@gmail.com>
2023-06-13 14:54:26 -0600

    first

---
+++ R/get_locations.R
@@ -0,0 +1,21 @@
+#' Get locations of a species
+#'
+#' @name get_locations
+#'
+#'
+#'
+#'
+#'
+#' @export
+
+library(spocc)
+library(sf)
+library(crgeo)
+library(crgrids)
+
+
+get_locations <- function(species_name) {
+  gbif_data <- occ(query = species_name, from = c("gbif", "inat", "ebird"), geometry = st_bbox(cr_outline), has_coord = TRUE)
+  df <- occ2df(gbif_data)
+  return(df)
+}
